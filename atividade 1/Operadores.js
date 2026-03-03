42ec34150e62a918aac15549191284d069987db2
FELIPE LIMA DUARTE <felipe.duarte@conab.gov.br>
2026-03-03 11:15:30 -0300

    Atividade 1, Operadores

---
+++ atividade 1/Operadores.js
@@ -0,0 +1,19 @@
+
+let numA = 10;
+let numB = 5;
+
+
+let resultadoSoma = numA + numB;
+console.log(`Resultado da soma entre ${numA} e ${numB} é ${resultadoSoma}`);
+
+
+let resultadoSubtracao = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${resultadoSubtracao}`);
+
+o
+let resultadoMultiplicacao = numA * num_;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${resultadoMultiplicacao}`);
+
+
+let resultadoDivisao = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${resultadoDivisao}`);
